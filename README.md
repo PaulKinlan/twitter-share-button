c814b4db3dce80d41168508aba77c5486c2ed250
Paul Kinlan <paul.kinlan@gmail.com>
2017-01-23 21:37:01 +0000

    First Commit of twitter share button

---
+++ README.md
@@ -0,0 +1,18 @@
+# Twitter Share Button
+
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/owner/my-element)
+
+<!--
+```
+<custom-element-demo>
+  <template>
+    <link rel="import" href="my-element.html">
+    <link rel="import" href="../other-element/other-element.html">
+    <next-code-block></next-code-block>
+  </template>
+</custom-element-demo>
+```
+-->
+```html
+<twitter-share-button></twitter-share-button>
+```
---
+++ twitter-share-button.js
@@ -0,0 +1,63 @@
+class QuickShareButton extends HTMLElement {
+  constructor() { super(); }
+
+  get text() {
+    return this.getAttribute('text');
+  }
+
+  set text(val) {
+    if(val) {
+      this.setAttribute('text', val);
+    } else {
+      this.removeAttribute('text');
+    }
+  }
+
+  get href() {
+    return this.getAttribute('href');
+  }
+
+  set href(val) {
+    if(val) {
+      this.setAttribute('href', val);
+    } else {
+      this.removeAttribute('href');
+    }
+  }
+
+}
+
+class TwitterShareButton extends QuickShareButton {
+
+  get template() {
+    if(this._template) return this._template;
+    else {
+      this._template = document.createElement('template');
+      let styles = document.createElement('style');
+      styles.innerHTML = `:host {}`;
+      let body = document.createElement('div');
+      body.innerHTML = `<button id="share-btn"><slot></slot></button>`;
+      this._template.content.appendChild(styles);
+      this._template.content.appendChild(body);
+      
+      return this._template;
+    }
+  }
+
+  constructor() {
+    super();
+    
+    const root = this.attachShadow({mode:'open'});
+    root.appendChild(this.template.content.cloneNode(true));
+
+    root.querySelector('#share-btn').addEventListener('click', e => {
+      let parentData = this.parentElement.shareData;
+    
+      let textValue = (parentData.text) ? parentData.text : (this.text || document.title);
+      let urlValue = (parentData.url) ? parentData.url : this.href;
+      location.href = `https://twitter.com/intent/tweet/?text=${encodeURIComponent(textValue)}&url=${encodeURIComponent(urlValue)}`;
+    });
+  }
+}
+
+customElements.define('twitter-share-button', TwitterShareButton);
